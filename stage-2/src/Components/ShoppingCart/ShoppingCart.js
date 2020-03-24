b5b66870c15b604e97e092864be206a26534fd9f
Li Jung Huang <lijunghuang@Lis-MacBook-Pro.local>
2020-03-24 16:10:49 -0700

    stage2

--- stage-2/src/Components/ShoppingCart/ShoppingCart.js
+++ stage-2/src/Components/ShoppingCart/ShoppingCart.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ShoppingCart.css";
 
-class ShoppingCart extends Component {
-  render() {
-    let shoppingCartDisplay = this.props.shoppingCart.map((element, index) => {
+function ShoppingCart (props){
+    let shoppingCartDisplay = props.cart.map((element, index) => {
       return (
         <div className="shopping-cart-product-container" key={index}>
           <img src={element.image} alt="" />
           <div className="shopping-cart-info">
-            <h2>{element}</h2>
+            <h2>{element.title}</h2>
             <h2>{"$" + element.price + ".00"}</h2>
             <div className="shopping-cart-button-container">
               <button
                 className="shopping-cart-button"
-                onClick={() => this.props.removeFromCart(element)}
+                onClick={() => props.removeFromCart(element)}
               >
                 Remove From Shopping Cart
               </button>
@@ -34,6 +33,5 @@ class ShoppingCart extends Component {
       </div>
     );
   }
-}
 
 export default ShoppingCart;
